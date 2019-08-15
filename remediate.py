ee7b1b4e328fbb137f5d348dd4d251cc42bdc813
Geoff Jones <geoff.jones@cyberis.co.uk>
2019-08-15 15:14:15 +0100

    Improved output

--- remediate.py
+++ remediate.py
@@ -13,11 +13,12 @@ parser.add_argument('--api',nargs='?',required=True,help="The API endpoint...e.g
 parser.add_argument('--file',nargs='?',required=True,help="The csv file containing remediation instructions")
 args = parser.parse_args()
 
+print("target_id,path,status,response")
 with open(args.file, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         json={"path":row["path"],"sign_off":row["sign_off"],"object_ids":row["object_ids"].split(),"reason":row["reason"],"password":row["password"]}
-        print(row["path"])
+        print("{},{}".format(row["target_id"],row["path"]),end='')
         response = requests.post("{api}/beta/targets/{target_id}/locations/{location_id}/remediation/{action}".format(api=args.api,target_id=row["target_id"],location_id=row["location_id"],action=row["action"]),json=json,auth=HTTPBasicAuth(args.username,args.password),verify=False)
-        print(response.status_code,response.text)
+        print(',{},"{}"'.format(response.status_code,response.text))
 
