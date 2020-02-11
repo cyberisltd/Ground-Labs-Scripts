a1e1caf11e3734b1a5a494e772f0f46103915744
Geoff Jones <geoff.jones@cyberis.co.uk>
2020-02-11 09:28:44 +0000

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -24,13 +24,14 @@ with open(args.file, newline='') as csvfile:
         if row["target_id"] not in targets:
             targets[row["target_id"]] = []
         targets[row["target_id"]].append({"id":row["location_id"],"subpath":row["subpath"]})
+        if row["location_id2"]:
+            targets[row["target_id"]].append({"id":row["location_id2"],"subpath":row["subpath2"]})
         
-
 for target in targets:
     for location in targets[target]:
         targetlist.append({"id": target,"locations":targets[target]})
 
-json={"label":args.label,"targets":targetlist,"profile":[args.profile],"start":args.start}
+json={"label":args.label,"targets":targetlist,"profiles":[args.profile],"start":args.start}
 
 response = requests.post("{api}/beta/schedules".format(api=args.api),json=json,auth=HTTPBasicAuth(args.username,args.password),verify=False)
 print('{},{}'.format(response.status_code,response.text))
